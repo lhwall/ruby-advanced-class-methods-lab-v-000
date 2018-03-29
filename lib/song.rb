eba31b9e2b639362e742e25c39ed430ad956666c
Lucy Wall <lhwall@gmail.com>
2018-03-29 18:15:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,8 +35,9 @@ class Song
   end
 
   def self.alphabetical
-    @@all.sort_by do |song| song
+    alphabetical_array = @@all.sort_by do |song| song
     end
+    alphabetical_array
   end 
 
   def self.all
